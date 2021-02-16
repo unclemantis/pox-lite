4612c16b7cf5ea4756bc07c09d02541d0b96d05c
Uncle Mantis <unclemantis@gmail.com>
2021-02-16 05:33:26 -0500

    ----

---
+++ app/deploy-contract.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deployContract = void 0;
+const { estimateContractDeploy, makeContractDeploy, broadcastTransaction, PostConditionMode } = require('@stacks/transactions');
+const { StacksTestnet } = require('@stacks/network');
+const fs = require("fs");
+const network = new StacksTestnet();
+const txOptions = {
+    contractName: 'pox-lite',
+    codeBody: fs.readFileSync('./contracts/pox-lite.clar').toString(),
+    senderKey: '4974a88497db2c415e5d7b16c27b229b2e883fbde6f1d2352319d06399ed094801',
+    network,
+    postConditionMode: PostConditionMode.Allow,
+};
+async function deployContract() {
+    const transaction = await makeContractDeploy(txOptions);
+    const txid = await broadcastTransaction(transaction, network);
+    console.log(txid);
+}
+exports.deployContract = deployContract;
+;
+deployContract();
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZGVwbG95LWNvbnRyYWN0LmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiZGVwbG95LWNvbnRyYWN0LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUFBLE1BQU0sRUFBRSxzQkFBc0IsRUFBRSxrQkFBa0IsRUFBRSxvQkFBb0IsRUFBRSxpQkFBaUIsRUFBRSxHQUFHLE9BQU8sQ0FBQyxzQkFBc0IsQ0FBQyxDQUFDO0FBQ2hJLE1BQU0sRUFBRSxhQUFhLEVBQUUsR0FBRyxPQUFPLENBQUUsaUJBQWlCLENBQUMsQ0FBQztBQUN0RCx5QkFBMEI7QUFHMUIsTUFBTSxPQUFPLEdBQUcsSUFBSSxhQUFhLEVBQUUsQ0FBQztBQUVwQyxNQUFNLFNBQVMsR0FBRztJQUNoQixZQUFZLEVBQUUsVUFBVTtJQUN4QixRQUFRLEVBQUUsRUFBRSxDQUFDLFlBQVksQ0FBQywyQkFBMkIsQ0FBQyxDQUFDLFFBQVEsRUFBRTtJQUNqRSxTQUFTLEVBQUUsb0VBQW9FO0lBQy9FLE9BQU87SUFDUCxpQkFBaUIsRUFBRSxpQkFBaUIsQ0FBQyxLQUFLO0NBQzNDLENBQUM7QUFFSyxLQUFLLFVBQVUsY0FBYztJQUNsQyxNQUFNLFdBQVcsR0FBRyxNQUFNLGtCQUFrQixDQUFDLFNBQVMsQ0FBQyxDQUFDO0lBRXhELE1BQU0sSUFBSSxHQUFHLE1BQU0sb0JBQW9CLENBQUMsV0FBVyxFQUFFLE9BQU8sQ0FBQyxDQUFDO0lBQzlELE9BQU8sQ0FBQyxHQUFHLENBQUMsSUFBSSxDQUFDLENBQUM7QUFDcEIsQ0FBQztBQUxELHdDQUtDO0FBQUEsQ0FBQztBQUVGLGNBQWMsRUFBRSxDQUFDIn0=
