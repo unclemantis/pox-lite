9f6ac73d5698eacc80a18367e246e6787da7ecf9
Uncle Mantis <unclemantis@gmail.com>
2021-02-15 20:38:02 -0500

    add a point where testing is erroring

--- contracts/pox-lite.clar
+++ contracts/pox-lite.clar
@@ -1,10 +1,2 @@
-(define-data-var deposit-id int 0)
-
-(define-map deposits { height: uint } { deposit-id: int, address: principal, amount: uint, memo: (optional (string-ascii 80))})
-
-(define-private (deposit (amount uint) (address principal) (memo (string-ascii 80)))
-    (var-set deposit-id (+ (var-get deposit-id) 1))
-    (map-insert deposits {height: block-height } { deposit-id: (var-get deposit-id), address: address, amount: amount, memo: memo}))
-
-(begin
-  (deposit u100 tx-sender "I am going to win"))
+(define-public (say-hi)
+   (ok "hello world"))
--- test/deploy-contract.ts
+++ test/deploy-contract.ts
@@ -3,11 +3,11 @@ const { Client, Provider, ProviderRegistry } = require('@blockstack/clarity');
 
 describe("deploy contract test suite", () => {
   let poxLiteClient = new Client;
-  let provider = new Provider;
+  let provider = Provider;
 
   before(async () => {
     provider = await ProviderRegistry.createProvider();
-    poxLiteClient = new Client("SP30JX68J79SMTTN0D2KXQAJBFVYY56BZJEYS3X0B.pox-lite", "pox-lite", provider);
+    poxLiteClient = new Client("SP3GWX3NE58KXHESRYE4DYQ1S31PQJTCRXB3PE9SB.pox-lite", "pox-lite", provider);
   });
 
   it("should have a valid syntax", async () => {
