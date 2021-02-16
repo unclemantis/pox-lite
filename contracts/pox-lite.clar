5868fe4cdfbf1de6c6b4f16cce2a29c0f06f5e2a
Uncle Mantis <unclemantis@gmail.com>
2021-02-16 01:14:35 -0500

    Got test unit to work. Now need to find why contract returning false

--- contracts/pox-lite.clar
+++ contracts/pox-lite.clar
@@ -5,12 +5,16 @@
 { deposit-id: int,
   address: principal,
   amount: uint,
-  memo: (optional (string-ascii 80))
-})
+  memo: (string-ascii 80)})
 
 (define-private (deposit (amount uint) (address principal) (memo (string-ascii 80)))
-  (var-set deposit-id (+ (var-get deposit-id) 1))
-  (map-insert deposits {height: block-height } { deposit-id: (var-get deposit-id), address: address, amount: amount, memo: memo}))
-
-(begin
-  (deposit u100 'SP3GWX3NE58KXHESRYE4DYQ1S31PQJTCRXB3PE9SB "I am going to win"))
+  (begin
+    (var-set deposit-id (+ (var-get deposit-id) 1))
+    (map-insert deposits
+    { height: block-height }
+    { deposit-id: (var-get deposit-id),
+      address: address,
+      amount: amount,
+      memo: memo})
+  )
+)
--- test/deploy-contract.ts
+++ test/deploy-contract.ts
@@ -1,5 +1,5 @@
 const { assert } = require('chai');
-const { Client, Provider, ProviderRegistry } = require('@blockstack/clarity');
+const { Client, Provider, ProviderRegistry, Result } = require('@blockstack/clarity');
 
 describe("deploy contract test suite", () => {
   var poxLiteClient = Client;
@@ -16,6 +16,15 @@ describe("deploy contract test suite", () => {
 
   it("should deploy", async () => {
     await poxLiteClient.deployContract();
+  });
+
+  it("map should set", async () => {
+    const tx = poxLiteClient.createTransaction({
+      method: { name: "deposit", args: ["u100", "SP3GWX3NE58KXHESRYE4DYQ1S31PQJTCRXB3PE9SB", "none"] }
+    });
+    await tx.sign("SP3GWX3NE58KXHESRYE4DYQ1S31PQJTCRXB3PE9SB")
+    const receipt = await poxLiteClient.submitTransaction(tx);
+    assert.isTrue(receipt.success);
   });
 
   after(async () => {
