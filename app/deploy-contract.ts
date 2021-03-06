9da200b47ea4af4482bfec84b1e0f5c531be0570
Uncle Mantis <unclemantis@gmail.com>
2021-03-06 03:09:51 -0500

    pox-lite. Still having issues with optional type

--- app/deploy-contract.ts
+++ app/deploy-contract.ts
@@ -25,7 +25,7 @@ deployContract();
 //  "mnemonic": "frog scene north know beach okay parent trouble loop thought coast paper know display nut buddy tent current joke monkey relax hip crumble crater",
 //  "keyInfo": {
 //    "privateKey": "a9e8cc8b2409f7b045183f41d38fcb943e54f74775098bcbdc1ba07011357c7a01",
-//    "address": "ST1FZ7JENZT9B9NYFH7NGG6S9MXZWVJNGST39NWFR",
+///    "address": "ST1FZ7JENZT9B9NYFH7NGG6S9MXZWVJNGST39NWFR",
 //    "btcAddress": "mpGJXzdQAUJaPmK4TC28xistR2SjkVSPrP",
 //    "index": 0
 //  }
